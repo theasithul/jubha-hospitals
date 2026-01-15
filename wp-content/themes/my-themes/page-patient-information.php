d91a38cd2b8751b85f6b68b5579352d26002cafb
theasithul <theasithul9@gmail.com>
2026-01-15 16:02:45 +0700

    patient information

---
+++ wp-content/themes/my-themes/page-patient-information.php
@@ -0,0 +1,30 @@
+<?php get_header(); ?>
+
+<section class="hero">
+    <div class="hero-overlay"></div>
+    <div class="hero-content container">
+        <h1>In Patient</h1>
+        <div class="breadcrumb">
+            <a href="">Home</a> / <a href="#">Patient Information</a> / In Patient
+        </div>
+    </div>
+</section>
+
+
+<section class="content container">
+    <div class="content-left">
+        <h2>Rooms & Services</h2>
+        <h4>At Almana Hospitals we are committed to provide quality care at the highest.</h4> <br>
+        <span>To ensure optimal patient experience and a convenient atmosphere for swift recovery, all our rooms are equipped with the latest advanced medical equipment and well trained medical staff along with the essential entertainment and comfort facilities needed by the patient.</span> <br><br>
+        <span>Other than the regular shared and private rooms, luxurious VIP suites are also available at AGH Khobar and AGH Jubail for the most premium healthcare experience.</span>
+    </div>
+
+    <div class="content-right">
+        <img src="http://jubha-hospitals.test/wp-content/uploads/2026/01/l_JAAN8436.jpg" alt="Visitor Desk">
+    </div>
+</section>
+
+
+
+
+<?php get_footer(); ?>
