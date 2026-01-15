6da5a0f8de2a393943d1af1ff8c7efb0235162c4
theasithul <theasithul9@gmail.com>
2026-01-15 07:41:07 +0700

    chnageeddd

--- wp-content/themes/my-themes/footer.php
+++ wp-content/themes/my-themes/footer.php
@@ -1,98 +1,4 @@
-<!-- <footer class="site-footer">
-    <div class="footer-top">
-        <div class="container footer-top-inner">
-            <div>
-                <h2>Stay in Touch</h2>
-                <p>Subscribe to our health newsletters and publications for the latest updates.</p>
-            </div>
-            <a href="#" class="btn-outline">Subscribe to Our Newsletter</a>
-        </div>
-    </div>
-
-    <div class="footer-main">
-        <div class="container">
-            <div class="footer-logo">
-                <img src="http://jubha-hospitals.test/wp-content/uploads/2026/01/brief6-removebg-preview.png" alt="Hospital Logo">
-            </div>
-            <div class="footer-columns">
-                <div class="footer-col">
-                    <h4>CONNECT WITH US</h4>
-                    <ul>
-                        <li><a href="#">Contact Us</a></li>
-                        <li><a href="#">Share Your Feedback</a></li>
-                        <li><a href="#">International Services</a></li>
-                        <li><a href="#">MyHealth</a></li>
-                        <li><a href="#">Get Our Apps</a></li>
-                    </ul>
-                <div class="footer-social">
-                        <a href="#"><i class="fa-brands fa-facebook"></i></a>
-                        <a href="#"><i class="fa-brands fa-x-twitter"></i></a>
-                        <a href="#"><i class="fa-brands fa-square-instagram"></i></a>
-                        <a href="#"><i class="fa-brands fa-youtube"></i></a>
-                        <a href="#"><i class="fa-brands fa-linkedin"></i></a>
-                </div>
-                </div>
-                <div class="footer-col">
-                    <h4>FIND CARE</h4>
-                    <ul>
-                        <li><a href="#">Find a Doctor</a></li>
-                        <li><a href="#">Find a Location</a></li>
-                        <li><a href="#">Immediate Care</a></li>
-                        <li><a href="#">Emergency Care</a></li>
-                        <li><a href="#">Make an Appointment</a></li>
-                        <li><a href="#">Medical Services</a></li>
-                        <li><a href="#">Clinical Trials</a></li>
-                    </ul>
-                </div>
-                <div class="footer-col">
-                    <h4>PATIENT RESOURCES</h4>
-                    <ul>
-                        <li><a href="#">Prepare for Your Visit</a></li>
-                        <li><a href="#">Visitor Guidelines</a></li>
-                        <li><a href="#">Patient Education</a></li>
-                        <li><a href="#">Billing & Insurance</a></li>
-                        <li><a href="#">Price Transparency</a></li>
-                        <li><a href="#">Support & Information</a></li>
-                        <li><a href="#">Wellness & Routine Care</a></li>
-                    </ul>
-                </div>
-                <div class="footer-col">
-                    <h4>DISCOVER OUR HOSPITAL</h4>
-                    <ul>
-                        <li><a href="#">About Us</a></li>
-                        <li><a href="#">Departments</a></li>
-                        <li><a href="#">News & Stories</a></li>
-                        <li><a href="#">For Health Professionals</a></li>
-                        <li><a href="#">Community & Equity</a></li>
-                        <li><a href="#">Contact Media Team</a></li>
-                        <li><a href="#">Donate</a></li>
-                    </ul>
-                </div>
-                <div class="footer-col">
-                    <h4>NEWSLETTER HOSPITAL</h4>
-                    <ul>
-                    <p>Dolor amet sit justo amet elitr clita <br> ipsum elitr est</p>
-           
-                    <form action="">
-                        <input type="email" name="email" placeholder="Your Email">
-                        <button type="submit">Sign up </button>
-                    </form>
-                
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="footer-bottom">
-        <p>© 2026 Hospital Jubha, All Right Reserved.</p>
-    </div>
-</footer> -->
-
-
-
 <footer class="site-footer">
-
-    <!-- TOP BAR -->
     <div class="footer-top">
         <div class="footer-top-inner">
             <div>
@@ -102,8 +8,7 @@
             <a href="#" class="btn-outline">Subscribe to Our Newsletter</a>
         </div>
     </div>
-
-    <!-- MAIN FOOTER -->
+    
     <div class="footer-main">
         <div class="container">
 
