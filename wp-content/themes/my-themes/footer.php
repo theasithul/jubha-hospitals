a1176ad15b43b6e6c1c6e2666d53b7308c9143e4
theasithul <theasithul9@gmail.com>
2026-01-15 15:46:29 +0700

    Patient infor

--- wp-content/themes/my-themes/footer.php
+++ wp-content/themes/my-themes/footer.php
@@ -1,14 +1,5 @@
 <footer class="site-footer">
-    <div class="footer-top">
-        <div class="container footer-top-inner">
-            <div>
-                <h2>Stay in Touch</h2>
-                <p>Subscribe to our health newsletters and publications for the latest updates.</p>
-            </div>
-            <a href="#" class="btn-outline">Subscribe to Our Newsletter</a>
-        </div>
-    </div>
-
+    
     <div class="footer-main">
         <div class="container">
             <div class="footer-logo">
