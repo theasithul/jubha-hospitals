0220876cffa8cfd7dabc1a6f072e02d95bab1cdd
theasithul <theasithul9@gmail.com>
2026-01-21 13:05:01 +0700

    change patient

--- wp-content/themes/my-themes/page-patient-information.php
+++ wp-content/themes/my-themes/page-patient-information.php
@@ -11,15 +11,15 @@
 </section>
 
 
-<section class="content container">
-    <div class="content-left">
+<section class="contents container">
+    <div class="contents-left">
         <h2>Rooms & Services</h2>
         <h4>At Almana Hospitals we are committed to provide quality care at the highest.</h4> <br>
         <span>To ensure optimal patient experience and a convenient atmosphere for swift recovery, all our rooms are equipped with the latest advanced medical equipment and well trained medical staff along with the essential entertainment and comfort facilities needed by the patient.</span> <br><br>
         <span>Other than the regular shared and private rooms, luxurious VIP suites are also available at AGH Khobar and AGH Jubail for the most premium healthcare experience.</span>
     </div>
 
-    <div class="content-right">
+    <div class="contents-right">
         <img src="http://jubha-hospitals.test/wp-content/uploads/2026/01/l_JAAN8436.jpg" alt="Visitor Desk">
     </div>
 </section>
